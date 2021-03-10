060e7c7de33db060f34e80f322c29465f91e175c
Fikri Fadlillah <sayafifa@gmail.com>
2021-03-10 18:40:12 +0700

    Fix column:

--- model/personal.go
+++ model/personal.go
@@ -8,8 +8,8 @@ import (
 type Personal struct {
 	Id              string       `json:"id"    valid:"required"`
 	Nama            string       `json:"nama"  valid:"required"`
-	Usia            int          `json:"nama"  valid:"required"`
-	TanggalLahir    time.Time    `json:"nama"  valid:"required"`
+	Usia            int          `json:"usia"  valid:"required"`
+	TanggalLahir    time.Time    `json:"tanggal_lahir"  valid:"required"`
 }
 
 
