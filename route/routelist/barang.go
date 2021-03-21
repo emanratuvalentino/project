aa41268e12d38325a484801bb9c845cd6e97998e
Gerin Bagaslino <gbljh46@gmail.com>
2021-03-21 14:55:19 +0700

    fix input

--- route/routelist/barang.go
+++ route/routelist/barang.go
@@ -9,7 +9,9 @@ import (
 func Barang(echo *echo.Group) {
 
 	echo.POST("/latihanbarang/simpan", peremajaan.SimpanBarang)
-	echo.POST("/latihanbarang/update/", peremajaan.UpdateBarang)
+	echo.POST("/latihanbarang/update", peremajaan.UpdateBarang)
 	echo.GET("/latihanbarang", peremajaan.GetBarang)
-	echo.PUT("/latihanbarang/update/:id", peremajaan.UpdateBarang)
+	echo.PUT("/latihanbarang/simpanbarang/", peremajaan.SimpanBarang)
+	echo.PUT("/latihanbarang/update", peremajaan.UpdateBarang)
+	echo.DELETE("/latihanbarang/delete", peremajaan.DeleteBarang)
 }
